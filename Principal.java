baebb6cac3bc47eb12ec7c5f281d986d281d642c
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-02-16 22:42:53 -0600

    Atualización clase principal

--- Principal.java
+++ Principal.java
@@ -29,46 +29,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Scanner teclado = new Scanner(System.in);
-        int opcion = 0;
         Archivo archivo = new Archivo();
         ArrayList<String> filasLeidas = new ArrayList<String>();
 
-        //menu para elegir el modo del radio
-        while(opcion != 5){
-            
-            System.out.println("Que modo desea utilizar? \n");
-            System.out.println("1. Radio \n"
-                            + "2. Reproduccion \n"
-                            + "3. Telefono \n"
-                            + "4. Productividad \n"
-                            + "5. Salir \n");
-            opcion = teclado.nextInt();
-            teclado.nextLine();
-
-
-            filasLeidas = archivo.leerTxt();
-            for (String fila : filasLeidas) {
-                System.out.println(fila);
-            }
-
-            //Si la opcion es uno
-            if(opcion == 1){
-
-            //Si la opcion es 2
-            }else if(opcion == 2){
-                
-            //Si la opcion es 3
-            }else if(opcion == 3){
-                
-            //Si la respuesta 4
-            }else if(opcion == 4){
-                
-            }else{
-                System.out.println("Escoje una opcion del 1 al 5\n");
-            }
-    
+        filasLeidas = archivo.leerTxt();
+        for (String fila : filasLeidas) {
+            System.out.println(fila);
         }
+
+        
+
     }
 
 }
