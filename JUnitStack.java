ef4f28fcc52c01f3ec5273c4ada3cd34f2ec39fa
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-02-19 16:49:29 -0600

    JUnit test, clases PostFixCalc y Stack

--- JUnitStack.java
+++ JUnitStack.java
@@ -1,37 +1,71 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class JUnitStack {
 
-	@Test
-	void testStack() {
-		fail("Not yet implemented");
-	}
+	ArrayList<Integer> lista = new ArrayList<>();
 
 	@Test
 	void testPush() {
-		fail("Not yet implemented");
+		int valorEsperado = 20;
+		lista.add(0, valorEsperado);
+		
+		assertEquals(valorEsperado, lista.get(0), 0);
 	}
 
 	@Test
 	void testPull() {
-		fail("Not yet implemented");
+		String valorEsperado = "Vacío";
+		String valorRetornado = "";
+		lista.add(0, 20);		
+		lista.clear();
+		if(lista.isEmpty()) {
+			valorRetornado = "Vacío";
+		}else {
+			valorRetornado = "No vácio";
+		}
+		
+		assertEquals(valorEsperado, valorRetornado, "");
 	}
 
 	@Test
 	void testPeek() {
-		fail("Not yet implemented");
+		int valorEsperado = 10;
+		lista.add(0, 10);
+		int valorRetornado = lista.get(0);
+		
+		assertEquals(valorEsperado, valorRetornado, 0);
 	}
 
 	@Test
 	void testCount() {
-		fail("Not yet implemented");
+		int valorEsperado = 5;
+		lista.add(10);
+		lista.add(11);
+		lista.add(12);
+		lista.add(13);
+		lista.add(14);
+		
+		int valorRetornado = lista.size();
+		
+		assertEquals(valorEsperado, valorRetornado, 0);
 	}
 
 	@Test
 	void testIsEmpty() {
-		fail("Not yet implemented");
+		String valorEsperado = "No vacío";
+		String valorRetornado = "";
+		lista.add(0, 20);	
+		if(lista.isEmpty()) {
+			valorRetornado = "Vacío";
+		}else {
+			valorRetornado = "No vacío";
+		}
+		
+		assertEquals(valorEsperado, valorRetornado, "");
 	}
 
 }
